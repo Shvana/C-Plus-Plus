827c319a55f9fc82db0b39b3637badcf863a26c2
Shvana <htsunemikuv9@gmail.com>
2021-01-21 19:26:31 -0600

    explain basic structure

---
+++ src/basic/explain_code_basic.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Shvana on 21/01/2021.
+//
+#include <iostream> //preprocessor
+
+int main() //This is the first line of the function main.
+{  //All the code in a function is enclosed between braces.
+    int answer {42}; // Defines answer with value 42
+
+
+    std::cout << "The answer to life, the universe, and everything is " << answer << std::endl; //This statement is spread over three lines.
+
+    return 0; //This statement ends the function main.
+}
